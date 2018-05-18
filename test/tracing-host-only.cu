2e7117eb15254ee13223f37ede1f7f233836c14b
Alexander Matz <alexander.matz@ziti.uni-heidelberg.de>
2018-05-18 17:50:43 +0200

    got test to working, has data race in it

--- test/tracing-host-only.cu
+++ test/tracing-host-only.cu
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "../lib/Common.h"
 
 extern "C" {
@@ -19,6 +20,19 @@ void __trace_stop(cudaStream_t stream);
   }\
 } while(0)\
 
+void add_trace(uint32_t *fronts, uint32_t *backs, uint64_t *traces,
+    int slot, uint64_t desc, uint64_t addr, uint64_t size) {
+
+  while (fronts[slot] >= SLOTS_SIZE) {}
+
+  size_t offset = slot * SLOTS_SIZE + fronts[slot]*3;
+  fronts[slot]++;
+  traces[offset + 0] = desc;
+  traces[offset + 1] = addr;
+  traces[offset + 2] = size;
+  backs[slot]++;
+}
+
 int main(int argc, char** argv) {
   // check if unified addressing is used, so that cudaHostGetDevicePtr is the
   // identity function.
@@ -31,17 +45,22 @@ int main(int argc, char** argv) {
   }
 
   setenv("MEMTRACE_PATTERN", "./test-trace-host", 1);
-  printf("touching trace\n");
   __trace_touch(NULL);
   printf("starting trace\n");
   __trace_start(NULL, "test");
-  printf("getting trace info\n");
   traceinfo_t info;
   __trace_fill_info(&info, NULL);
+
+  uint32_t *fronts = info.front;
+  uint32_t *backs = info.back;
+  uint64_t *traces = info.slot;
+
+  for (int i = 0; i < 256; ++i) {
+    add_trace(fronts, backs, traces, 0, 3*i, 3*i + 1, 3*i + 2);
+  }
+
   printf("stopping trace\n");
   __trace_stop(NULL);
-  printf("stopped trace\n");
-
   cudaChecked(cudaStreamSynchronize(NULL));
 
   return 0;
